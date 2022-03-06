5c51c6c45dac93a7ad9cae2f865d242dd6266518
aswin-hub <69479534+aswin-hub@users.noreply.github.com>
2022-03-06 06:45:11 +0530

    'initial'

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "@material-ui/core";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <footer>
+        <small
+          style={{
+            fontSize: "10px",
+          }}
+        >
+          Copyright © 2022 aswinv-dev
+        </small>
+      </footer>
+    </Container>
+  );
 };
 
 export default Footer;
